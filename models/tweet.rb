0bd927bbc49e00fb89f86d8caf91260c7421e64a
tily <tidnlyam@gmail.com>
2014-10-04 18:46:05 +0900

    add visible attr to tweet

--- models/tweet.rb
+++ models/tweet.rb
@@ -6,6 +6,7 @@ class Tweet
 	field :published, type: Boolean
 	field :published_at, type: Time
 	field :status_id, type: Integer
+	field :visible, type: Boolean, default: true
 	validates :body, length: {maximum: 140}
 	belongs_to :user
 end
